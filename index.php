fa9e47b084b315b89642979f03c3c4c34a49fc2c
adsi7698 <adsi7698@gmail.com>
2017-11-21 14:03:31 +0530

    debug index.php

--- index.php
+++ index.php
@@ -275,22 +275,4 @@ session_start();
 		</div>
 	</div>
 </body>
-</html><div id="loggedInModal" class="modal fade" role="dialog">
-		<div class="modal-dialog">
-
-			<!-- Modal Content -->
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal">&times;</button>
-					<h3 class="modal-title">Welcome!</h3>
-				</div>
-				<div class="modal-body">
-					<h4>You are logged in!</h4>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				</div>
-			</div>
-
-		</div>
-	</div>
+</html>
